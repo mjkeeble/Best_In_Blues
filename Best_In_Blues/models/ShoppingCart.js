4a26b5cd06ad75afa5f66d456900cd184e3d7749
Mark J. Keeble <mjkeeble@yahoo.de>
2020-11-20 15:59:29 +0100

    shoppingCart model expanded

--- Best_In_Blues/models/ShoppingCart.js
+++ Best_In_Blues/models/ShoppingCart.js
@@ -6,9 +6,28 @@ const shoppingCartSchema = new Schema(
     {
         articles: {
             type: Schema.Types.ObjectId,
-            ref: ShopArticle
+            ref: ShopArticle,
+            quantity: Number
         },
-        quantity: number
+        billing_firstname: String,
+        billing_lastname:String,
+        billing_street: String,
+        billing_zip: String,
+        billing_city: String,
+        billing_state: String,
+        billing_telephone: String,
+        billing_email: String,
+        billing_country: String,
+        checkout_usernote:String,
+        delivery_firstname: String,
+        delivery_lastname:String,
+        delivery_street: String,
+        delivery_zip: String,
+        delivery_city: String,
+        delivery_state: String,
+        delivery_telephone: String,
+        delivery_email: String,
+        delivery_country: String,
     };
 
 });
