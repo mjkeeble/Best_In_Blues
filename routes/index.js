18cee068ea12a1b11af001aa45f4b47cecbce79e
Mark J Keeble <mjkeeble@yahoo.de>
2020-11-23 15:05:30 +0100

    navigation routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/shop', (req, res, next) =>{
+  res.render('shop/shopArticles');
+});
+
+router.get('/projects', (req, res, next) => {
+  console.log(`calling projects`);  
+  res.render('projects');
+});
+
+router.get('/gigs', (req, res, next) => {
+  res.render('gigs');
+});
+
+router.get('/news', (req, res, next) => {
+  res.render('news');
+});
+
+router.get('/classes', (req, res, next) => {
+  res.render('classes');
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact');
+});
+
 module.exports = router;
